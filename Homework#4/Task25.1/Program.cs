82cc9deb53588b9082ce021f0a943aea24474a3f
Darya <daryaplesk@gmail.com>
2022-09-13 15:16:58 +0300

    Task25 completed

---
+++ Homework#4/Task25.1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// Решение через цикл for.
+
+double Input(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToDouble(System.Console.ReadLine());
+}
+
+double Exponentiate(double x, double y)
+{
+    double result = x;
+    if (y == 0) return 1;
+    if (y > 0)
+    {
+        for (int i = 2; i <= y; i++)
+        { 
+            result = result * x;
+        }
+        return result;
+    }
+    else 
+    {
+        y = Math.Abs(y);
+        for (int i = 2; i <= y; i++)
+        { 
+            result = result * x;
+        }
+        return 1 / result;
+    }
+}
+
+double A = Input("Введите число: ");
+double B = Input("Введите степень, в котороую будет возведено число: ");
+System.Console.WriteLine($"{A}^{B} = {Exponentiate(A, B)}");
---
+++ Homework#4/Task25/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// Решение через Math.Pow.
+
+double Input(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToDouble(System.Console.ReadLine());
+}
+
+double A = Input("Введите число: ");
+double B = Input("Введите степень, в котороую будет возведено число: ");
+System.Console.WriteLine($"{A}^{B} = {Math.Pow(A, B)}");
