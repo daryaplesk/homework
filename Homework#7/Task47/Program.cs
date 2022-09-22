4d425d8a1c9f3d6f3da25b5ef9ec8022b066d103
Darya <daryaplesk@gmail.com>
2022-09-22 17:31:32 +0300

    Task47 completed

---
+++ Homework#7/Task47/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int InputData(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] FillArrayRandomly(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i , j] = new Random().Next(-100, 100);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i , j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+} 
+
+int m = InputData("Введите m: ");
+int n = InputData("Введите n: ");
+
+if ( m <= 0 || n <= 0) System.Console.WriteLine("Введите корректные данные");
+
+int[,] array = new int[m , n];
+FillArrayRandomly(array);
+PrintArray(array);
