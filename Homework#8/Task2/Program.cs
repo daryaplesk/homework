af6699e4ca769733ab496dfaff13bad7a8e3628f
Darya <daryaplesk@gmail.com>
2022-09-27 16:22:36 +0300

    Tasks completed

---
+++ Homework#8/Task2/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 2: Задайте две квадратные матрицы одного размера. Напишите программу, которая будет находить произведение двух матриц.
+// Для справки: 
+// Как умножать матрицы: https://ru.onlinemschool.com/math/assistance/matrix/multiply/?oms_all=a%3d%7b%7b4,4,5%7d,%7b1,5,2%7d,%7b9,9,7%7d%7d,b%3d%7b%7b5,8,1%7d,%7b4,4,9%7d,%7b5,3,2%7d%7d
+
+int[,] CreateAndFillArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+int[,] MultiplyMatrices(int[,] array1, int[,] array2)
+{
+    int[,] result = new int[array1.GetLength(0), array2.GetLength(1)];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            for (int k = 0; k < array2.GetLength(0); k++)
+            {
+                result[i, j] += array1[i, k] * array2[k, j];
+            }
+        }
+    }
+    return result;
+}
+
+int[,] array1 = CreateAndFillArray(3, 3);
+PrintArray(array1);
+int[,] array2 = CreateAndFillArray(3, 3);
+PrintArray(array2);
+int[,] result = MultiplyMatrices(array1, array2);
+PrintArray(result);
---
+++ Homework#8/Task3/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 3. Сформируйте двухмерный массив из неповторяющихся случайных двузначных чисел (размер массива не более 50 элементов). Напишите программу, которая будет построчно выводить массив.
+
+bool CheckNumberInArray(int[,] array, int number)
+{
+    int check = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == number)
+            {
+                check = 1;
+                break;
+            }
+        }
+    }
+    return (check == 1);
+}
+
+int[,] FillArrayWithNonRepeatingNumbers(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    Random rnd = new Random();
+    int temporary = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            do
+                temporary = rnd.Next(9, 99);
+            while (CheckNumberInArray(array, temporary) == true);
+            array[i, j] = temporary;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] array = FillArrayWithNonRepeatingNumbers(3, 3);
+PrintArray(array);
